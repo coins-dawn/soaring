c28de8e91f733f2c8a1657a04d91975d0421e95f
igarashi339 <igarashi339@gmail.com>
2025-10-23 17:21:33 +0900

    save

--- soaring/best_combus_stop_sequences.py
+++ soaring/best_combus_stop_sequences.py
@@ -1,5 +1,58 @@
 import json
-import os
+import random
+from itertools import combinations
+from ortools.constraint_solver import routing_enums_pb2
+from ortools.constraint_solver import pywrapcp
+
+random.seed(42)
+
+def solve_tsp(duration_matrix):
+    """OR-toolsを使用して巡回セールスマン問題を解く"""
+    manager = pywrapcp.RoutingIndexManager(len(duration_matrix), 1, 0)
+    routing = pywrapcp.RoutingModel(manager)
+
+    def duration_callback(from_index, to_index):
+        from_node = manager.IndexToNode(from_index)
+        to_node = manager.IndexToNode(to_index)
+        return duration_matrix[from_node][to_node]
+
+    transit_callback_index = routing.RegisterTransitCallback(duration_callback)
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.first_solution_strategy = (
+        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
+    )
+
+    solution = routing.SolveWithParameters(search_parameters)
+    
+    if not solution:
+        return None, float('inf')
+
+    # 解の取得
+    route = []
+    index = routing.Start(0)
+    total_distance = 0
+    while not routing.IsEnd(index):
+        route.append(manager.IndexToNode(index))
+        previous_index = index
+        index = solution.Value(routing.NextVar(index))
+        total_distance += duration_matrix[manager.IndexToNode(previous_index)][manager.IndexToNode(index)]
+    route.append(manager.IndexToNode(index))
+    assert(route)
+    
+    return route, total_distance
+
+
+def create_duration_matrix(stops, duration_dict):
+    """所要時間の行列を作成"""
+    n = len(stops)
+    matrix = [[0] * n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if i != j:
+                matrix[i][j] = duration_dict.get((stops[i], stops[j]), float('inf'))
+    return matrix
 
 
 def load_combus_stops():
@@ -27,7 +80,33 @@ def generate_combus_stop_sequence(
     combus_duration_dict: dict[tuple[str, str], int],
     duration_limit: int,
 ) -> list[str]:
-    return []
+    """バス停シーケンスを生成"""
+    current_stops = []
+    best_sequence = []
+    
+    # 初期の3点をランダムに選択
+    while True:
+        current_stops = random.sample(combus_stops, 3)
+        duration_matrix = create_duration_matrix(current_stops, combus_duration_dict)
+        route, total_duration = solve_tsp(duration_matrix)
+        if total_duration <= duration_limit:
+            break
+    
+    # サイズを1つずつ大きくする
+    while True:
+        duration_matrix = create_duration_matrix(current_stops, combus_duration_dict)
+        route, total_duration = solve_tsp(duration_matrix)
+        if total_duration <= duration_limit:
+            # duration_limitを超えていない場合、ひとつふやす
+            best_sequence = [current_stops[i] for i in route[:-1]]
+            remaining_stops = [s for s in combus_stops if s not in current_stops]
+            if not remaining_stops:
+                return best_sequence         
+            next_stop = random.choice(remaining_stops)
+            current_stops.append(next_stop)
+        else:
+            # duration_limitを超えた場合、ひとつ前のシーケンスを返す
+            return best_sequence
 
 
 def best_combus_stops_for_single_duration_limit(
@@ -40,7 +119,7 @@ def best_combus_stops_for_single_duration_limit(
         combus_stops, combus_duration_dict, duration_limit
     )
 
-    return []
+    return combus_stop_sequence
 
 
 def write_best_combus_stop_sequences(best_combus_sequences: dict, output_path: str):
